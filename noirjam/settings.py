5f3c353dffb9bb97886130e1c42936ed395ddbc2
Paul Williams <paul.d.williamsxiv@gmail.com>
2021-01-20 10:38:31 -0500

    altered allowed host

--- noirjam/settings.py
+++ noirjam/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ["https://noirjam.herokuapp.com"]
+ALLOWED_HOSTS = ["noirjam.herokuapp.com"]
 
 
 # Application definition
