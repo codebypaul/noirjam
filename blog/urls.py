ae27430ed66b9b8d5e3b1083fad5417a50954c69
Paul Williams <paul.d.williamsxiv@gmail.com>
2021-01-20 09:57:06 -0500

    created social media site

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+)
+from . import views
+
+urlpatterns = [
+    # path("", views.home, name="blog_home"),
+    path("blog/", PostListView.as_view(), name="actually_blog"),
+    path("post/<int:pk>", PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("user/<str:username>", UserPostListView.as_view(), name="user-posts"),
+    path("post/new", PostCreateView.as_view(), name="post-create"),
+    path("about/", views.about, name="blog_about"),
+]
